542a36bb54a9853ec6111f9de065ecaa2f0f01af
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-05-31 16:27:45 +0500

    Chapter8

---
+++ 20250531-162717.py
@@ -0,0 +1,9 @@
+
+
+
+# 8-2. Favorite Book
+def favorite_book(title):
+    print(f"One of my favorite books is {title}.")
+
+# Call the function with a specific book title
+favorite_book("Alice in Wonderland")

74dffe20e2e9c1ac2046d7fc7d9bc21dad833afa
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-05-31 16:26:01 +0500

    Update 20250531-162045.pychapter8

--- 20250531-162045.py
+++ 20250531-162045.py
@@ -1,6 +1,15 @@
-# 8-1. Display Message
+Question 1_______________
+8.1Write a function called
+display_message() that prints one
+sen￾tence telling everyone what
+you are 
+learning about in this chapter
+. Call the function, and make sure 
+the message displays correctly.
+
+solution __________________
+
 def display_message():
     print("In this chapter, I am learning how to define and call functions in Python.")
-
-# Call the function
+Call the function
 display_message()

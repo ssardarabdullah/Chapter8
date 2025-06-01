85513241b381e80b561b78481f0087f9b139c4a5
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 15:17:37 +0500

    Update 20250601-151200.pychapter8

--- 20250601-151200.py
+++ 20250601-151200.py
@@ -1,18 +1,29 @@
-# printing_functions.py
+Question 6________
+Printing Models: Put the functions 
+    for the example printing_models.py 
+in a separate file called printing_
+functions.py. Write an import 
+statement at the top of printing
+_models.py, and modify the file to 
+use the imported functions. 
 
+solution ____________
 def print_models(unprinted_designs, completed_models):
     """Simulate printing each design, until none are left."""
     while unprinted_designs:
-        current_design = unprinted_designs.pop()
-        print(f"Printing model: {current_design}")
-        completed_models.append(current_design)
+        current_design = unprinted
+        _designs.pop()
+        print(f"Printing model: 
+        {current_design}")
+        completed_models.append(
+            current_design)
 
 def show_completed_models(completed_models):
     """Show all the models that were printed."""
     print("\nThe following models have been printed:")
     for model in completed_models:
         print(model)
-        # printing_models.py
+        
 
 import printing_functions
 

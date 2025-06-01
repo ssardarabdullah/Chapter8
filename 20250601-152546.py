fec8d531430e368dcbfad13cb29bcfee596c4059
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 15:31:23 +0500

    Update 20250601-152546.pychapter8

--- 20250601-152546.py
+++ 20250601-152546.py
@@ -1,6 +1,14 @@
+Question 8_______
+ Make a list containing a series of short
+text messages. Pass the 
+list to a function called
+show_messages(), which prints each
+text message.
+
+
+solution __________
 def show_messages(messages):
     for message in messages:
         print(message)
-
 messages = ["Hello!", "How are you?", "See you soon.", "Have a great day!"]
 show_messages(messages)

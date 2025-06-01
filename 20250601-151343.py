30fdcb6bc49212ab2a724054e07e9b5316ff2e64
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 15:18:58 +0500

    Chapter8

---
+++ 20250601-151343.py
@@ -0,0 +1,16 @@
+# greetings.py
+
+def greet_user(name):
+    print(f"Hello, {name}!")
+    # main_program1.py
+import greetings
+
+greetings.greet_user("Alice")
+# main_program1.py
+import greetings
+
+greetings.greet_user("Alice")
+# main_program4.py
+import greetings as gr
+
+gr.greet_user("Diana")

0e13986a30a50ac6b9a6ee1f52f86df4226d2109
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 15:35:50 +0500

    Chapter8

---
+++ 20250601-152635.py
@@ -0,0 +1,14 @@
+def send_messages(messages, sent_messages):
+    while messages:
+        current_message = messages.pop(0)
+        print(f"Sending message: {current_message}")
+        sent_messages.append(current_message)
+
+original_messages = ["Hello!", "How are you?", "See you soon.", "Have a great day!"]
+sent_messages = []
+
+# Use a copy of the original list to preserve the original
+send_messages(original_messages[:], sent_messages)
+
+print("\nOriginal messages list (after copy was used):", original_messages)
+print("Sent messages list:", sent_messages)

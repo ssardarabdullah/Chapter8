c055b37ec8e8f1ab1b1a2dce22711a8aaf2440d9
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 15:27:33 +0500

    Chapter 8

---
+++ 20250601-152546.py
@@ -0,0 +1,6 @@
+def show_messages(messages):
+    for message in messages:
+        print(message)
+
+messages = ["Hello!", "How are you?", "See you soon.", "Have a great day!"]
+show_messages(messages)

83c3163228b74e85d772fee1e054507db2f3c452
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 14:54:28 +0500

    Update 20250601-145105.pychapter 8

--- 20250601-145105.py
+++ 20250601-145105.py
@@ -1,8 +1,20 @@
-def describe_city(city, country='USA'):
-    """Prints a sentence stating which country the city is in."""
-    print(f"{city} is in {country}.")
+Question 3________________
+Write a function called describe_
+city() that accepts the name of
+a city and its country.
+The function should print a simple
+sentence, such as
+Reykjavik is in Iceland. Give the 
+parameter for the country a default
+value.
+Call your function for three
+different cities,
 
-# Calling the function for three different cities
+solution _____________
+def describe_city(city, country='USA'):
+    """Prints a sentence stating which
+    country the city is in."""
+    print(f"{city} is in {country}.")
 describe_city('Reykjavik', 'Iceland')
 describe_city('Paris', 'France')
-describe_city('New York')  # Uses default country 'USA'
+describe_city('New York')  

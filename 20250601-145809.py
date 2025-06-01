fca2b1bddcf04d18074ba723c411adf03789a192
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 14:58:46 +0500

    Chapter8

---
+++ 20250601-145809.py
@@ -0,0 +1,15 @@
+def show_messages(messages):
+    for message in messages:
+        print(message)
+
+# List of short text messages
+text_messages = [
+    "Hey, how are you?",
+    "Don't forget the meeting at 10am.",
+    "Lunch later?",
+    "Happy birthday!",
+    "Call me when you're free."
+]
+
+# Call the function with the list
+show_messages(text_messages)
